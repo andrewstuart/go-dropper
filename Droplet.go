650c646b233cc45ee1c7570e682aaf97b8d2e95a
Andrew Stuart <andrew.stuart2@gmail.com>
2014-12-12 20:50:07 -0700

    Update droplet struct

--- Droplet.go
+++ Droplet.go
@@ -2,8 +2,15 @@ package main
 
 type DropletSize string
 
+type SSHKey string
+
 type Droplet struct {
-	Name   string      `json:"name"`
-	Region RegionSlug  `json:"region"`
-	Size   DropletSize `json:"size"`
+	Name              string      `json:"name"`
+	Region            RegionSlug  `json:"region"`
+	Size              DropletSize `json:"size"`
+	Backups           bool        `json:"backups"`
+	IPv6              bool        `json:"ipv6"`
+	SshKeys           []SSHKey    `json:"ssh_keys,omitempty"`
+	PrivateNetworking bool        `json:"private_networking"`
+	UserData          string      `json:"user_data,omitempty"`
 }
