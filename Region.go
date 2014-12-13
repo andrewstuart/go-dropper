b758498e0478baf8d845b79d28636468b8822bdb
Andrew Stuart <andrew.stuart2@gmail.com>
2014-12-12 19:58:11 -0700

    Remove unused stringers

--- Region.go
+++ Region.go
@@ -16,26 +16,7 @@ type Region struct {
 	Sizes     []string `json:"sizes"`
 }
 
-// func (r Region) String() string {
-// 	avail := ""
-// 	if r.Available {
-// 		avail = "Not available"
-// 	} else {
-// 		avail = "Available"
-// 	}
-// 	return fmt.Sprintf("%s\t(%s):\t%s", r.Name, r.Slug, avail)
-// }
-
 //RegionResp is a wrapper for the region responses
 type RegionResp struct {
 	Regions []Region `json:"regions"`
 }
-
-// func (r RegionResp) String() string {
-// 	s := ""
-// 	for _, r := range r.Regions {
-// 		s += fmt.Sprintf("%s\n", r)
-// 	}
-
-// 	return s
-// }
